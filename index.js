73445d49908fbb9b53a3855bab9a523a4951e33a
prajotsurey <prajotsurey@gmail.com>
2021-01-15 11:37:08 +0530

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { response, request } = require('express')
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -26,7 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
-
+app.use(morgan('tiny'))
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
