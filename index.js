c82d362ab4e79f24ecd7bd8386e443de40240b61
prajotsurey <prajotsurey@gmail.com>
2021-01-15 08:48:49 +0530

    Phonebook backend step4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response, request } = require('express')
 const express = require('express')
 const app = express()
 
@@ -29,16 +30,33 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/info', (request, response) => {
-    let personCount = persons.length
-    let firstLine = "Phonebook has info for " + personCount + " people"
+    const personCount = persons.length
+    const firstLine = `Phonebook has info for ${personCount} people`
     
-    let date = new Date()
-    let time = `${date}`
+    const date = new Date()
+    const time = `${date}`
     console.log(date)
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.end(firstLine +'\n\n'+ time)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+    
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server running on prot ${PORT}`)
